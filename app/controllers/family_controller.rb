2b667246618d3242c11659f5c44ae038c38864cd
johntrandall <john@johnrandall.com>
2014-06-13 16:51:10 -0400

    working on family controller

--- app/controllers/family_controller.rb
+++ app/controllers/family_controller.rb
@@ -1,23 +1,102 @@
 class FamilyController < ApplicationController
-  
-  def index
-  	#FIX
-  	render html: "user should never see this. This route should be deleted when done. Try the show page at /family/id_num"
-  end
+	
+	def index
+		#FIX
+		render html: "user should never see this. This route should be deleted when done. Try the show page at /family/id_num"
+	end
 
 
-  def new
-  end
+	def new
+	end
 
-  def show
-  	# @family = Family.find(current_user.family_id)
-  	@family = Family.find(1)
-  	# json_fam = @family.to_json
-  	# binding.pry
-  	# render json: JSON.pretty_generate(@family.to_json)
-  	render json: @family
-  end
+	def show
+		# @family = Family.find(current_user.family_id)
+		# payload = {}
 
-  def edit
-  end
+		@slice_point = 'CUT ME HERE'
+
+		@family = Family.last
+
+		@persons = @family.persons
+		@children = @family.children
+		@caregivers = @family.caregivers
+			@luddites = @family.luddites
+			@users = @family.users
+
+		# @calendar_entries = @family.calendar_entries
+			# @appointments = @family.appointments
+			# @childcares = @family.childcares
+			# @happenings = @family.happenings
+
+		# build_payload
+		#   @persons = @family.persons
+		#   @children = @family.children
+		#   @caregivers = @family.caregivers
+		#     @luddites = @family.luddites
+		#     @users = @family.users
+
+		#   @calendar_entries = @family.calendar_entries
+		#     @appointments = @family.appointments
+		#     @childcares = @family.childcares
+		#     @happenings = @family.happenings
+
+		@payload = build_payload()
+		# puts ""
+		# puts ""
+		# puts ""
+		# puts ""
+		# pp @payload
+		# pp JSON.parse(@payload)
+		
+
+		render json: 'see terminal'
+
+	end
+
+
+
+	def edit
+	end
+
+
+
+	def build_payload
+		payload_arr = []
+
+		self.instance_variables.each do |instance_variable|
+			single_item_hash = {instance_variable: self.instance_variable_get(instance_variable)}
+			payload_arr << single_item_hash
+		end
+
+		slice_point_index = payload_arr.find_index(@slice_point)
+		pp slice_point_index
+		
+		payload_arr.slice!(0, slice_point_index)
+
+		pp payload_arr
+
+
+
+
+
+
+		# payload = {}
+
+		# self.instance_variables.each do |instance_variable|
+		# 	# puts instance_variable
+		# 	# puts instance_variable.to_s
+		# 	payload[instance_variable]= self.instance_variable_get(instance_variable)
+		# 	# pp self.instance_variable_get(instance_variable)
+
+		# end
+
+		return payload
+	
+	end
+# 
+	# def instance_variables_hash
+			# @payload = Hash[instance_variables.map { |name| [name, instance_variable_get(name)] } ]
+	# end
+
+
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,9 +1,5 @@
 class Person < ActiveRecord::Base
-	
 
-	## TRAVERSING THE DATA
-
-		### FAMILY CONNECTION
 
 		belongs_to :family
 
@@ -14,46 +10,6 @@ class Person < ActiveRecord::Base
 				has_many :happenings, through: :participations
 
 
-	## CALENDAR ENTRIES
-
-		### Getters
-
-		### dynamic call
-		# def calendar_entries_of_type(type)
-		# 	if type == 'all' || type === nil
-		# 		return all_callendar_entries()
-		# 	else
-		# 		return calendar_entries.where(type: type)
-		# 	end
-		# end
-
-			# ### alternates
-			# def all_callendar_entries
-			# 	calendar_entries
-			# end
-
-			# def childcares
-			# 	calendar_entries.where(type: 'Childcare')
-			# end
-			# 		# def childcare
-			# 		# 	childcares
-			# 		# end
-
-			# def appointments
-			# 	calendar_entries.where(type: 'Appointment')
-			# end
-			# 		# def appointment
-			# 		# 	appointments
-			# 		# end
-
-			# def happenings
-			# 	calendar_entries.where(type: 'Happening')
-			# end
-			# 		# def happening
-			# 		# 	happenings
-			# 		# end
-		
-		### Setters
 
 
 
